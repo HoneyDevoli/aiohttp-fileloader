853fd3917b31826270310728db9bf62650178639
Oleg Svistunov <devoli911@gmail.com>
2020-10-06 18:42:32 +0300

    refactored daemon implementation

--- file_loader/daemon/daemon.py
+++ file_loader/daemon/daemon.py
@@ -7,21 +7,114 @@ from abc import ABC, abstractmethod
 from pathlib import Path
 
 
-def _fork():
-    pid = os.fork()
-    if pid > 0:
-        # exit from parent
-        sys.exit(0)
+class PidFile:
+    """Class for work with pid file.
+    :param file_path: the pid`s file path """
+    def __init__(self, file_path: Path):
+        self.file_path = file_path
 
-    return pid
+    def load(self) -> int:
+        """Loaded pid from file.
+        :raise FileNotFoundError when pid file by file_path not found
+        :raise ValueError when pid has incorrect format
+        :return: pid from file
+        """
+        with open(self.file_path, 'r') as pf:
+            pid = pf.read().strip()
+            if not pid.isdigit():
+                raise ValueError('Pid file does not contain a number')
+
+            pid = int(pid)
+            if not pid > 0:
+                raise ValueError('Pid must be greater than 0')
+
+            return pid
+
+    def save(self, value: int) -> None:
+        """Save pid in file.
+        :param value: the pid that should be save in file
+        :raise ValueError when pid has incorrect format
+        """
+        if not value > 0:
+            raise ValueError('Cannot be negative')
+
+        with open(self.file_path, 'w') as f:
+            f.write(str(value))
+
+    def remove(self) -> None:
+        """Delete pid file."""
+        if self.is_file_exist():
+            os.remove(self.file_path)
+
+    def is_file_exist(self) -> bool:
+        """Checking that the file by the path is exist."""
+        return os.path.exists(self.file_path)
+
+
+class Process:
+    """Class for working with unix process.
+    :param file_path: the pid`s file path which the process id
+    will be place"""
+    def __init__(self, file_path: Path):
+        self.file_path = file_path
+        self.pid_file = PidFile(file_path)
+
+    def is_running(self) -> bool:
+        """Checking process status.
+        :raise FileNotFoundError when pid file by file_path not found
+        :return: true then the process is running
+        """
+        if self.pid_file.is_file_exist():
+            pid = self.pid_file.load()
+            if not os.path.exists(f'/proc/{pid}'):
+                self.pid_file.remove()
+                return False
+        else:
+            return False
+
+        return True
+
+    def stop(self) -> None:
+        """Try killing the daemon process by id.
+        :raise FileNotFoundError when pid file by file_path not found
+        :raise ValueError when pid has incorrect format"""
+        pid = self.pid_file.load()
+        os.kill(pid, signal.SIGTERM)
+
+    def save_pid(self) -> None:
+        """Save current pid in file."""
+        pid = os.getpid()
+        self.pid_file.save(pid)
+
+    def remove_pid(self):
+        """Remove file with pid."""
+        self.pid_file.remove()
+
+    def get_pid(self):
+        """Load pid from file.
+        :raise FileNotFoundError when pid file by file_path not found"""
+        self.pid_file.load()
+
+    @staticmethod
+    def fork() -> int:
+        """Fork current process.
+        :raise OSError when an error occurred during the fork process
+        :return: return new pid of the child process
+        """
+        pid = os.fork()
+        if pid > 0:
+            # exit from parent
+            sys.exit(0)
+
+        return pid
 
 
 class AbstractDaemon(ABC):
     """A generic daemon class. For use subclass should override
-     the run() method
+     the run() method.
     :param storage: working directory for daemon
-    :param pid_file_name: the pid`s file name which the daemon process id
-    will be place
+    :param pid_file_name: the name of the pid file that will store the
+        process ID
     :param stdin: new place for stdin
     :param stdout: new place for stdout
     :param stderr: new place for stderr
@@ -41,7 +134,7 @@ class AbstractDaemon(ABC):
         self.stdout = stdout
         self.stderr = stderr
         self.storage = storage
-        self.pid_file = self.storage / pid_file_name
+        self.process = Process(self.storage / pid_file_name)
 
         for k in kwargs.keys():
             setattr(self, k, kwargs[k])
@@ -49,7 +142,7 @@ class AbstractDaemon(ABC):
     def daemonize(self) -> None:
         """Daemonize class. UNIX double fork mechanism."""
         try:
-            _fork()
+            Process.fork()
         except OSError as err:
             sys.stderr.write(f'fork #1 failed: {err}')
             sys.exit(1)
@@ -61,7 +154,7 @@ class AbstractDaemon(ABC):
 
         # do second fork
         try:
-            _fork()
+            Process.fork()
         except OSError as err:
             sys.stderr.write(f'fork #2 failed: {err}')
             sys.exit(1)
@@ -76,61 +169,42 @@ class AbstractDaemon(ABC):
         os.dup2(so.fileno(), sys.stdout.fileno())
         os.dup2(se.fileno(), sys.stderr.fileno())
 
-        # write pid_file
-        atexit.register(self.del_pid)
+        atexit.register(self.process.remove_pid)
 
-        pid = str(os.getpid())
-        with open(self.pid_file, 'w+') as f:
-            f.write(pid + '\n')
-
-    def del_pid(self) -> None:
-        """Delete pid file"""
-        if os.path.exists(self.pid_file):
-            os.remove(self.pid_file)
+        self.process.save_pid()
 
     def start(self) -> None:
         """Start the daemon."""
-
-        # Check for a pid_file to see if the daemon already runs\
-        if os.path.exists(self.pid_file):
-            sys.stderr.write(f"pid_file {self.pid_file} already exist. "
-                             f"Daemon already running?\n")
+        if self.process.is_running():
+            sys.stderr.write("Daemon already running\n")
             sys.exit(1)
         else:
             self.daemonize()
             self.run()
 
-    def stop(self):
+    def stop(self) -> None:
         """Stop the daemon.
-            :raise OSError: process termination error
+        :raise OSError: process termination error
         """
-
-        # Get the pid from the pid_file
         try:
-            with open(self.pid_file, 'r') as pf:
-                pid = int(pf.read().strip())
+            if self.process.is_running():
+                self.process.stop()
         except FileNotFoundError:
-            sys.stderr.write(f"pid file {self.pid_file} does not exist."
-                             f" Daemon not running?\n")
+            sys.stderr.write(f'Pid file in storage \'{self.storage}\' does not'
+                             f' exist. Daemon not running?\n')
             sys.exit(1)
-        else:
-            # Try killing the daemon process
-            try:
-                os.kill(pid, signal.SIGTERM)
-            except OSError as err:
-                if err.errno == errno.ESRCH:
-                    # ESRCH == No such process
-                    self.del_pid()
-                    sys.exit(1)
-                elif err.errno == errno.EPERM:
-                    # EPERM clearly means there's a process to deny access to
-                    sys.stderr.write('Error stop daemon with id {}.'
-                                     ' Deny access'.format(pid))
-                    sys.exit(1)
-                else:
-                    raise
+        except OSError as err:
+            if err.errno == errno.ESRCH:
+                # ESRCH == No such process
+                sys.stderr.write('The daemon has already stopped')
+                sys.exit(1)
+            elif err.errno == errno.EPERM:
+                # EPERM clearly means there's a process to deny access to
+                sys.stderr.write(f'Error stop daemon with '
+                                 f'id {self.process.get_pid()}. Deny access.')
+                sys.exit(1)
             else:
-                self.del_pid()
+                raise
 
     def restart(self):
         """Restart the daemon."""
